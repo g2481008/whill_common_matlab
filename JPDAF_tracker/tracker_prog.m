952cef847b368d51a81ea0ad278326943395f66d
tokada <166749204+g2481011@users.noreply.github.com>
2025-10-09 13:59:32 +0900

    Update tracker_prog.m
    初期値の設定方法を変更．mex化のために引数を変更．
    
    Signed-off-by: tokada <166749204+g2481011@users.noreply.github.com>

--- JPDAF_tracker/tracker_prog.m
+++ JPDAF_tracker/tracker_prog.m
@@ -1,9 +1,11 @@
-function [confirmed,tentative,alltracks,info,detection,detection_forplot] = tracker_prog(observation,currentInfo,pms)
+function [confirmed,tentative,alltracks,info,detection] = tracker_prog(observation,time,MeasurementNoise)
     persistent tracker measParams
     if isempty(tracker)
         measParams = struct('HasVelocity',false);
-        observation = [1000;1000;0.1]';
         tracker = trackerJPDA('FilterInitializationFcn',@initcvekf,'TrackLogic','History','ConfirmationThreshold',[10 30],'DeletionThreshold',[20 40],'AssignmentThreshold',[100 300],'ClutterDensity',1e-10,'MaxNumTracks',500,'MaxNumTracksPerCluster',1,'MaxNumDetectionsPerCluster',5,'EnableMemoryManagement',true,'InitializationThreshold',0.1,'TimeTolerance',0.1,'DetectionProbability',0.8,'OOSMHandling','Retrodiction','MaxNumOOSMSteps',3,'MaxNumDetectionsPerSensor',1000);%'MaxNumEvents',5,,'MaxNumDetections',Inf,'MaxNumDetectionsPerSensor',1000
+        if isempty(observation)
+            observation = [1000;1000;0.1]';
+        end
     end
     num_de = size(observation,1);
     detection = cell(num_de,1);
@@ -11,10 +13,8 @@ function [confirmed,tentative,alltracks,info,detection,detection_forplot] = trac
         detection = {};
     else
         for i = 1:num_de
-            detection{i} = objectDetection(currentInfo.time,observation(i,1:3)','MeasurementNoise',pms.MeasurementNoise,'ObjectAttributes',struct('myProperty',i),'MeasurementParameters',measParams);
+            detection{i} = objectDetection(time,observation(i,1:3)','MeasurementNoise',MeasurementNoise,'ObjectAttributes',struct('myProperty',i),'MeasurementParameters',measParams);
         end
     end
-    detection_forplot = detection;
-    detection = detection(~cellfun(@isempty, detection));
-    [confirmed,tentative,alltracks,info] = tracker(detection,currentInfo.time);
+    [confirmed,tentative,alltracks,info] = tracker(detection,time);
 end
